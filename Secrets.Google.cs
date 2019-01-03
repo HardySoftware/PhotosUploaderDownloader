2be6ae0cab05434a18d6dbd4b71e051651a61eb2
Hardy Wang <hardywang@outlook.com>
2019-01-02 20:03:52 -0500

    Add template for secrets.cs.

---
+++ Secrets.Google.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * This is a template file for HardySoft.PhotosUploaderDownloader.Google\Security\Secret.cs, which is not included in the version control system.
+ * You need to get your own values from https://console.developers.google.com/, then copy the content to the file as described above.
+ */
+
+namespace HardySoft.PhotosUploaderDownloader.Google.Security
+{
+    /// <summary>
+    /// A class to have the secrets used to communicate with Google Api.
+    /// </summary>
+    /// <remarks>Please visit https://console.developers.google.com/apis/credentials to create your own credential and replace values in this class.</remarks>
+    internal static class Secrets
+    {
+        /// <summary>
+        /// The Google credential client Id.
+        /// </summary>
+        public const string ClientId = "your-value-goes-here";
+
+        /// <summary>
+        /// The Google credential client secret.
+        /// </summary>
+        public const string ClientSecret = "your-value-goes-here";
+    }
+}
