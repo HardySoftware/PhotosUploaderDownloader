7a7c8dcde1a42d9fbae3e8356bbf4137ffd7d4d1
Hardy Wang <hardywang@outlook.com>
2019-01-02 22:02:35 -0500

    Enable unit test in build pipeline.

---
+++ HardySoft.PhotosUploaderDownloader.Google.Test/GooglePhotosLibraryClient/ConstructorTests.cs
@@ -0,0 +1,16 @@
+﻿namespace HardySoft.PhotosUploaderDownloader.Google.Test.GooglePhotosLibraryClient
+{
+    using System;
+    using PhotoService;
+    using Xunit;
+
+    public class ConstructorTests
+    {
+        [Fact]
+        public void Null_ApiClient_Throws()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new GooglePhotosLibraryClient(null));
+            Assert.Equal("apiClient", ex.ParamName);
+        }
+    }
+}
