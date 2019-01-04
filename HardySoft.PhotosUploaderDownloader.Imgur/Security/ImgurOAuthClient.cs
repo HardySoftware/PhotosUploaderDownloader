808e5bd0d8d93759a7ad247d3c02614cad24961a
Hardy Wang <hardywang@outlook.com>
2019-01-03 21:38:55 -0500

    Add a dummy file to forece to create security folder.

---
+++ HardySoft.PhotosUploaderDownloader.Imgur/Security/ImgurOAuthClient.cs
@@ -0,0 +1,9 @@
+﻿namespace HardySoft.PhotosUploaderDownloader.Imgur.Security
+{
+    /// <summary>
+    /// An OAuth client to authenticate Imgur services.
+    /// </summary>
+    public class ImgurOAuthClient
+    {
+    }
+}
