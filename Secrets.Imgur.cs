cc59eafd3f23e7cd3d20d05e82b1eb55173b438a
Hardy Wang <hardywang@outlook.com>
2019-01-03 21:36:46 -0500

    Fix template file content error

--- Secrets.Imgur.cs
+++ Secrets.Imgur.cs
@@ -1,12 +1,12 @@
 ﻿/*
- * This is a template file for HardySoft.PhotosUploaderDownloader.Google\Security\Secret.cs, which is not included in the version control system.
+ * This is a template file for HardySoft.PhotosUploaderDownloader.Imgur\Security\Secret.cs, which is not included in the version control system.
  * You need to get your own values from https://api.imgur.com/#registerapp, then copy the content to the file as described above.
  */
 
 namespace HardySoft.PhotosUploaderDownloader.Imgur.Security
 {
     /// <summary>
-    /// A class to have the secrets used to communicate with Google Api.
+    /// A class to have the secrets used to communicate with Imgur Api.
     /// </summary>
     /// <remarks>Please visit https://api.imgur.com/#registerapp to create your own credential and replace values in this class.</remarks>
     internal static class Secrets
@@ -14,11 +14,11 @@ namespace HardySoft.PhotosUploaderDownloader.Imgur.Security
         /// <summary>
         /// The Google credential client Id.
         /// </summary>
-        public const string ClientId = "398102ddaf93d96";
+        public const string ClientId = "your-value-goes-here";
 
         /// <summary>
         /// The Google credential client secret.
         /// </summary>
-        public const string ClientSecret = "28d18dff0e77e21f4dda25a9302cfd2edf71f6a9";
+        public const string ClientSecret = "your-value-goes-here";
     }
 }
